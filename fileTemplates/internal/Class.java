f942569ebe32ed6fc30d5721c55abecd6c3327da
pengc <pengchong328@163.com>
2020-01-13 15:52:44 +0800

    IU-2019.3.1 <79061@PC-DELL Create baseRefactoring.xml, markdown.xml, ide.general.xml, git.xml, web-types-registry.xml, project.default.xml, rainbow_brackets.xml, equalsHashCodeTemplates.xml, lombok-plugin.xml, yiiguxing.translation.xml, Default.xml, find.xml, ui.lnf.xml, filetypes.xml, UserDefine.xml, keymap.xml, androidEditors.xml, plugin_ui.xml, colors.scheme.xml, databaseDrivers.xml, templates.xml, GrepConsole.xml, Default.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, Eclipse copy.xml, wsl.distributions.xml, vcs.xml, codota.xml, mybatis.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @ClassName: ${NAME}
+ * @Date:      ${DATE} ${TIME}
+ * @version:   V1.0
+ * @author:  ${USER}
+ */ 
+public class ${NAME} {
+}
